3702c9b7f47b107d8d14a78dd9b62811954bf422
Bunker <bunkergrenader@gmail.com>
2019-04-29 13:01:53 -0700

    Added cool ASCII header and options helper.

--- Linux_Enumeration.sh
+++ Linux_Enumeration.sh
@@ -7,6 +7,32 @@
 # Okay, so first we need to layout the sections of what we're trying to enumerate. Feel free to add more. I figure the current four is a decent amount? I know there are plenty of other
 # Linux Enumeration scripts out there with more but I want to see what we can create on our own and with what we know should be checked.
 
+# Cool ASCII text because... Why not?
+ascii()
+{
+    cat << "EOF"
+         __    _                     ______                                      __            
+        / /   (_)___  __  ___  __   / ____/___  __  ______ ___  ___  _________ _/ /_____  _____
+       / /   / / __ \/ / / / |/_/  / __/ / __ \/ / / / __ `__ \/ _ \/ ___/ __ `/ __/ __ \/ ___/
+      / /___/ / / / / /_/ />  <   / /___/ / / / /_/ / / / / / /  __/ /  / /_/ / /_/ /_/ / /    
+     /_____/_/_/ /_/\__,_/_/|_|  /_____/_/ /_/\__,_/_/ /_/ /_/\___/_/   \__,_/\__/\____/_/     
+EOF
+}
+
+# Define help options of -h and -?
+help()
+{
+    echo "There are currently no other options other than -h and -? (help) for now. Usage of this script only requires you to run it."
+}
+
+while getopts ":h?" opt; do
+    case ${opt} in
+        h ) help; exit;;
+        ? ) help; exit;;
+    esac
+done
+
+ascii
 
 # System Information
 
