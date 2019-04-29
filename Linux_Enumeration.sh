bee961a6c4088f393adb6fd7d9461a3607d24072
Bunker <Justin.M.Bunker@uspsector.com>
2019-04-29 08:18:32 -0700

    Initial Script

---
+++ Linux_Enumeration.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# This script is intended to help users in an offensive and defensive manner when trying to enumerate a local linux machine for permissions issues.
+# This could include misconfigured file permissions, user permissions, sudo privileges, etc. 
+# After finding an issue, the script will attempt to provide advice for remediation (Dynamic or Static?)
+
