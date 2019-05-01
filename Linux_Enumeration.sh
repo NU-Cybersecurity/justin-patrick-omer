0139913820f619954a96ac973d059c4018ba2ae4
student <student@student.local>
2019-04-30 20:22:18 -0700

    Forgot to add newline, woops

--- Linux_Enumeration.sh
+++ Linux_Enumeration.sh
@@ -37,12 +37,12 @@ ascii
 # System Information
 # -Patrick- "Basic sysinfo summary. Plan on adding a check of the current version against available kernel updates and alerting if there's a new kernel update available."
 printf "System Summary"
-printf "$(uname -s)"
-printf "$(uname -o)"
-printf "$(uname -n)"
-printf "$(uname -r)"
-printf "$(uname -v)"
-printf "$(uname -m)"
+printf "$(uname -s) \n"
+printf "$(uname -o) \n"
+printf "$(uname -n) \n"
+printf "$(uname -r) \n"
+printf "$(uname -v) \n"
+printf "$(uname -m) \n"
 
 # User Information
 
