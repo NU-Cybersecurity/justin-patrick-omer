811a7f7be23c58d5db1f3e0d8179f9a1925730ae
PringerZxZ <41497092+PringerZxZ@users.noreply.github.com>
2019-05-01 19:09:23 -0700

    Formatting adjustment

--- Linux_Enumeration.sh
+++ Linux_Enumeration.sh
@@ -36,43 +36,43 @@ ascii
 
 
 printf "\n \n"
-printf "	SYSTEM SUMMARY \n"
-printf "Hostname:				$(uname -n) \n"
+printf "		SYSTEM SUMMARY \n"
+printf "Hostname:			$(uname -n) \n"
 printf "Operating System:		$(uname -o) \n"
 printf "Distribution:			$(sed -n '/DISTRIB_ID/p' /etc/*-release | awk -F "=" '{print $2}')"
 printf "Kernal Name: 			$(uname -s) \n"
 printf "Kernel Release:			$(uname -r) \n"
 printf "Kernel Version: 		$(uname -v) \n"
-printf "System Architecture:	$(uname -m) \n"
+printf "System Architecture:		$(uname -m) \n"
 printf "\n \n"
 
 #Network Information
-printf "	NETWORK INTERFACE SUMMARY \n"
+printf "		NETWORK INTERFACE SUMMARY \n"
 printf "$( ip addr show | sed -n '/valid/!p' | awk -F " " '{print $2}')"
 printf "\n"
 
 #DNS Information
-printf "	DNS SERVER SUMMARY \n"
+printf "		DNS SERVER SUMMARY \n"
 printf "$(sed -n '/nameserver/p' /etc/resolv.conf  | awk -F" " '{print $2}')"
 printf "\n"
 
 # User Information
 #Print username in /etc/passwd, excluding those with the /sbin/nologin or /bin/false
-printf "	USER ACCOUNTS WITH BASH ACCESS \n" 
+printf "		USER ACCOUNTS WITH BASH ACCESS \n" 
  printf "$(cat /etc/passwd | sed -n '/false/!p' | sed -n '/nologin/!p' | awk -F":" '{print $1}')"
  printf "\n"
  # Print all other users (i.e. service accounts)
- printf "	USER ACCOUNTS WITHOUT BASH ACCESS 'i.e. Service Accounts' \n"
+ printf "		USER ACCOUNTS WITHOUT BASH ACCESS 'i.e. Service Accounts' \n"
  printf "$(cat /etc/passwd | sed -n '/bash/!p' | awk -F":" '{print $1}')"
  printf "\n"
  
 
- #Check who has sudo access
+#Check who has sudo access
 USER="printf "$(cat /etc/passwd | awk -F":" '{print $1}')""
 GROUP="printf "$(cat /etc/group | awk -F":" '{print $1}')""
 printf "\n"
-printf "        CHECKING SUDOERS FILE \n"
-printf "        USER ACCOUNTS WITH SUDO CAPABILITIES \n \n"
+printf "        	CHECKING SUDOERS FILE \n"
+printf "        	USER ACCOUNTS WITH SUDO CAPABILITIES \n \n"
 for i in $USER
 do
 USERSUDO="$(grep -e "$i"  /etc/sudoers | sed -n '/#/!p' | sed -n '/User_Alias/!p' | sed -n '/root/!p' | sed -n '/bin/!p')"
@@ -82,7 +82,7 @@ printf "$i \n"
 fi
 done
 printf "\n"
-printf "        GROUP ACCOUNTS WITH SUDO CAPABILITIES \n \n"
+printf "        	GROUP ACCOUNTS WITH SUDO CAPABILITIES \n \n"
 for i in $GROUP
 do
 GROUPSUDO="$(grep -e "$i" /etc/sudoers | sed -n '/#/!p' | sed -n '/bin/!p' | sed -n '/User_Alias/!p' | sed -n '/root/!p')"
@@ -99,11 +99,11 @@ for i in $USER
 do
 
 
-# Check if any users/groups can SUDO up without re-authenticating
+# Cchoheck if any users/groups can SUDO up without re-authenticating
 NOPASSWD="$(sed -n '/NOPASSWD/p' /etc/sudoers | sed -n '/#/!p' | sed -n '/root/!p' | sed -n "/$i/p" | awk '{print $1}')"
 if [ "$NOPASSWD" != "" ]
 then
-printf "        WARNING \nUser $i SUDOERS configuration contains NOPASSWD \n"
+printf "		WARNING \nUser $i SUDOERS configuration contains NOPASSWD \n"
 else
 continue
 fi
@@ -113,7 +113,7 @@ do
 NOPASSWD="$(sed -n '/NOPASSWD/p' /etc/sudoers | sed -n '/#/!p' | sed -n '/root/!p' | sed -n "/$i/p" | awk '{print $1}')"
 if [ "$NOPASSWD" != "" ]
 then
-printf "        WARNING \nGroup $i SUDOERS configuration contains NOPASSWD \n"
+printf "		WARNING \nGroup $i SUDOERS configuration contains NOPASSWD \n"
 else
 continue
 fi
@@ -125,10 +125,10 @@ done
 DISTRO="$(sed -n '/DISTRIB_ID/p' /etc/*-release | awk -F "=" '{print $2}')"
 if [ "$DISTRO" == "Red Hat" ] && [ "$DISTRO" == "Centos" ]
 then
-printf "Distribution = Red Hat or Centos, checking Systemctl. \n"
+printf "		Distribution = Red Hat or Centos, checking Systemctl. \n"
 printf "$(systemctl | sed -n '/service/p' | sed -n '/active/p' | awk -F"." '{print $1}')"
 else 
-printf "Distribution = Ubuntu, Debian, or Other, checking Service. \n '?' = Status Unknown \n '+' = Status Running "
+printf "		Distribution = Ubuntu, Debian, or Other, checking Service. \n		 	'?' = Status Unknown \n		 	'+' = Status Running \n "
 printf "$(service --status-all | sed -n '/+/p')"
 fi
 
