018689722570bdecf7e4cde4e9490bd809098ef5
root <root@DESKTOP-FG9TOL8.localdomain>
2019-05-05 20:56:11 -0700

    Converted from dos to unix language to remedy error attempting to run functions on Ubuntu

--- Linux_Enumeration.sh
+++ Linux_Enumeration.sh
@@ -1,10 +1,10 @@
-#! /bin/bash
+#!/bin/bash
+# AUTHORS: Justin B, Patrick W, Omer
 # This script is intended to help users in an offensive and defensive manner when trying to enumerate a local linux machine for permissions issues.
 # This could include misconfigured file permissions, user permissions, sudo privileges, etc. 
 # After finding an issue, the script will attempt to provide advice for remediation (Dynamic or Static?)
 # Okay, so first we need to layout the sections of what we're trying to enumerate. Feel free to add more. I figure the current four is a decent amount? I know there are plenty of other
 # Linux Enumeration scripts out there with more but I want to see what we can create on our own and with what we know should be checked.
-# Cool ASCII text because... Why not?
 ascii()
 {
     cat << "EOF"
